9bd31c6c4860abb9f8e0778a3045436b11df7130
guill <guill@DESKTOP-N4B4I01.home>
2018-05-14 21:23:37 +0200

    Commit initial du tpfilrouge

---
+++ tpfilrouge/WebContent/inc/menu.jsp
@@ -0,0 +1,6 @@
+<%@ page pageEncoding="utf-8" %>
+<fieldset>
+	<legend>Menu</legend>
+	<a href="<c:url value="/creationClient" />">Créer un client</a><br>
+	<a href="<c:url value="/creationCommande" />">Créer une commande</a><br>
+</fieldset>
---
+++ tpfilrouge/src/com/pge/servlets/CreationCommande.java
@@ -0,0 +1,49 @@
+package com.pge.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.pge.beans.Commande;
+import com.pge.forms.CreationCommandeForm;
+
+public class CreationCommande extends HttpServlet {
+    /**
+     * 
+     */
+    private static final long  serialVersionUID = 1526257567954287841L;
+    public static final String ATT_COMMANDE     = "commande";
+    public static final String ATT_FORM         = "form";
+
+    public static final String VUE_SUCCES       = "/WEB-INF/afficherCommande.jsp";
+    public static final String VUE_FORM         = "/WEB-INF/creerCommande.jsp";
+
+    public void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
+        /* À la réception d'une requête GET, simple affichage du formulaire */
+        this.getServletContext().getRequestDispatcher( VUE_FORM ).forward( request, response );
+    }
+
+    public void doPost( HttpServletRequest request, HttpServletResponse response )
+            throws ServletException, IOException {
+        /* Préparation de l'objet formulaire */
+        CreationCommandeForm form = new CreationCommandeForm();
+
+        /* Traitement de la requête et récupération du bean en résultant */
+        Commande commande = form.creerCommande( request );
+
+        /* Ajout du bean et de l'objet métier à l'objet requête */
+        request.setAttribute( ATT_COMMANDE, commande );
+        request.setAttribute( ATT_FORM, form );
+
+        if ( form.getErreurs().isEmpty() ) {
+            /* Si aucune erreur, alors affichage de la fiche récapitulative */
+            this.getServletContext().getRequestDispatcher( VUE_SUCCES ).forward( request, response );
+        } else {
+            /* Sinon, ré-affichage du formulaire de création avec les erreurs */
+            this.getServletContext().getRequestDispatcher( VUE_FORM ).forward( request, response );
+        }
+    }
+}
