b276d74f0a37928a21ffc738c05c1e87c61fec1c
Philou73 <guillemette.philippe@gmail.com>
2018-05-18 22:20:07 +0200

    Ajout de la suppression des commandes passées par un client qu'on supprime.

--- tpfilrouge/src/com/pge/servlets/SuppressionClient.java
+++ tpfilrouge/src/com/pge/servlets/SuppressionClient.java
@@ -1,7 +1,9 @@
 package com.pge.servlets;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -9,7 +11,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import com.pge.beans.Client;
+import com.pge.beans.*;
+
 
 public class SuppressionClient extends HttpServlet {
 	/**
@@ -17,6 +20,7 @@ public class SuppressionClient extends HttpServlet {
 	 */
 	private static final long serialVersionUID = 1L;
 	private final static String SESSION_CLIENTS = "listeClients";
+	private final static String SESSION_COMMANDES = "listeCommandes";
 	private final static String ATT_LISTE_CLIENTS = "listeClients";
 	private final static String ATT_SUPPRESSION = "suppression";
 	private final static String VUE_LISTE = "/listerClients";
@@ -29,8 +33,25 @@ public class SuppressionClient extends HttpServlet {
 		if (listeClients.get(client) != null) {
 			listeClients.remove(client);	
 		}
+		
+		// Suppression des commandes passées par les clients
+		Map<String, Commande> listeCommandes = (Map<String, Commande>) session.getAttribute(SESSION_COMMANDES);
+		Map<String, Commande> newListeCommandes = new HashMap<String, Commande>();
+		Set<String> keySet = listeCommandes.keySet();
+		
+		//listeCommandes.elements();
+		for (String key : keySet ) {
+			Commande commande = listeCommandes.get(key);
+			
+			if (! commande.getClient().getNom().equals(client)) {
+				newListeCommandes.put(commande.getDate(), commande);
+			}
+		}
 		// On replace la liste des clients en session
 		session.setAttribute(SESSION_CLIENTS, listeClients);
+		
+		// On replace la liste des commandes en session
+		session.setAttribute(SESSION_COMMANDES, newListeCommandes);
 		
 		// On affiche la vue listerClients.jsp en redirection
 		response.sendRedirect(request.getContextPath() + VUE_LISTE);
